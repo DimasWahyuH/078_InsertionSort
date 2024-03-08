524ebae7c54c621d124190dd1dcc2d89359f956d
DimasWahyuH <dimaswh19@gmail.com>
2024-03-08 16:46:16 +0700

    menambahkan proses input

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -4,3 +4,25 @@ using namespace std;
 
 int arr[20];    //Deklarasi variabel array a dengan ukuran 20
 int n;       // Deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
+
+void input() { //procedur untuk input
+	int d;
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array : "; // output ke layar
+		cin >> n;      // Input dari pengguna
+		if (n <= 20)   // Jika n kurang dari atau sama dengan 20
+			break;     // Keluar dari loop
+		else {         // Jika n lebih dari 20 
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // output ke layar
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan elemen array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << " : ";
+		cin >> arr[i];
+	}
+}
